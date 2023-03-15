ddd016834cad12417aa5e522b13019791d5dd84d
jawsfett <joseph-mark.triolo@uts.edu.au>
2023-03-15 13:01:11 +1100

    First steps

---
+++ CompareFilesData.py
@@ -0,0 +1,22 @@
+import os
+
+HOME_DIR = os.path.abspath(os.pardir)
+
+OldPath = HOME_DIR+"\\Techdebt-Project\\OldFile.txt"
+NewPath = HOME_DIR+"\\Techdebt-Project\\NewFile.txt"
+
+if os.path.exists(OldPath) and os.path.exists(NewPath):
+    
+    OldFileObj = open(OldPath, mode = 'r')
+    NewFileObj = open(NewPath, mode = 'r')
+
+    OldList = OldFileObj.read().split('\n')
+    NewList = NewFileObj.read().split('\n')
+
+    OldList.sort()
+    NewList.sort()
+
+    if OldList == NewList: 
+        print("Files are the same.")
+    else:
+        print("Files are not the same.")
